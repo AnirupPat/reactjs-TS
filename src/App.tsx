2e8b668d76e72caee7c2c7e94a7b76163e7a9317
Anirup <anirup049@gmail.com>
2021-07-25 15:18:24 +0530

    removing commented code

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,6 @@ import Todos from './components/Todos';
 import TodoContextProvider from './store/todo-context';
 
 function App() {
-  // const [todos, setTodos] = useState<Todo[]>([]);
-  // const addTodoHandler = (todoText: string) => {
-  //   const newTodo = new Todo(todoText);
-  //   setTodos(prevState => {
-  //     return [...prevState, newTodo]
-  //     //prevState.concat(newTodo);
-  //   });
-  // }
-
-  // const removeTodoHandler = (id: string) => {
-  //   setTodos(prevTodo => {
-  //     return prevTodo.filter(todo => todo.id !== id);
-  //   })
-  // }
-  
   return (
     <TodoContextProvider>
       <NewTodo />
